39a2b3c2e02db5094c6bf2a25c2dcec9e6faafdf
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2021-10-27 12:30:00 +0100

    Added inline styles to default ButtonGroup
    Signed-off-by: Joshua Baker <160484+joshuabaker@users.noreply.github.com>

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,6 +1,21 @@
 import React, { forwardRef } from "react";
+import { getButtonGroupJustifyContent } from "../utils/helpers";
 import { BaseComponent } from "./BaseComponent";
+import { usePageContext } from "./PageContext";
 
 export const ButtonGroup = forwardRef((props, ref) => {
-  return <BaseComponent ref={ref} baseClassName={"button-group"} {...props} />;
+  const page = usePageContext();
+  const buttonsPosition = page.settings.buttonsPosition;
+
+  return (
+    <BaseComponent
+      ref={ref}
+      baseClassName={"button-group"}
+      style={{
+        display: "flex",
+        justifyContent: getButtonGroupJustifyContent(buttonsPosition),
+      }}
+      {...props}
+    />
+  );
 });
